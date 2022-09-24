c1db2dc237bae2376b2f3c3559964315c9d411c4
aungphone-mm <system334@gmail.com>
2022-09-24 23:23:38 +0630

    test

--- insert.php
+++ insert.php
@@ -13,8 +13,29 @@
 server with default setting (user 'root' with no password) */
 //$link = mysqli_connect("localhost", "aungphone", "admin", "shopygndb");
 //$link = mysqli_connect("localhost", "root", "", "shopygndb");
-$link = mysqli_connect("localhost", "root", "", "shopygndb");
- 
+
+$host= "ec2-52-200-5-135.compute-1.amazonaws.com";
+$dbname = "dbk5cubscsr3i3";
+$user = "oixsjrlzkdzkqq";
+$password = "91556de58737f1eeda4089bcca863261f3dd2687187e3a0b7044c53a43d4c2cf";
+$port = "5432";
+
+try{
+  //Set DSN data source name
+    $dsn = "pgsql:host=" . $host . ";port=" . $port .";dbname=" . $dbname . ";user=" . $user . ";password=" . $password . ";";
+
+
+  //create a pdo instance
+  $pdo = new PDO($dsn, $user, $password);
+  $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
+  $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+echo 'Connection failed: ' . $e->getMessage();
+}
+//db
+
 session_start();
 // Check connection
 if($link === false){
