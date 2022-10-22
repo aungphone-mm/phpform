1d29858be810ce7dd082d15a0610eb7c62c3082b
Aung Phone <13567891+aungphone-mm@users.noreply.github.com>
2022-10-22 20:33:19 +0630

    Update delete.php

--- delete.php
+++ delete.php
@@ -11,11 +11,8 @@
 /* Attempt MySQL server connection. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 //$link = mysqli_connect("localhost", "aungphone", "admin", "shopygndb");
- $link = mysqli_connect("localhost", "root", "", "shopygndb");
-// Check connection
-if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
-}
+include('connect.php');
+	
  $del = mysqli_real_escape_string($link, $_REQUEST['delete']);
  $first_name = mysqli_real_escape_string($link, $_REQUEST['first_name']);
 // Attempt select query execution
